b9bf2851636121955df24d4e96cca86da5873ca8
Matthieu <matthieuroussel6@gmail.com>
2021-10-08 14:51:20 +0200

    Factoriel

--- workspace/factoriel.c
+++ workspace/factoriel.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+typedef short TypeEntier;
+
+TypeEntier factorielle(TypeEntier n){
+    int produit = 1;
+    for(int k = 0; k<n ; k++ ) {
+        produit = produit*(n-k);
+    }
+    return produit;
+}
+
+void afficheFactorielle(void) {
+    int N =0;
+    printf("Un nombre entier :");
+    scanf("%d", &N);
+    printf("%d! = %lld\n", N, factorielle(N));
+}
+
 int main(void){
-    printf("hello");
+    afficheFactorielle();
     return 0;
 }
